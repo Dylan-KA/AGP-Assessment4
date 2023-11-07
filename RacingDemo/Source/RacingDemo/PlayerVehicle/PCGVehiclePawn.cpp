53e33b9d1f8e7f37552878340616ae4d8ee50d55
Dylan Archer <111679436+Dylan-KA@users.noreply.github.com>
2023-11-07 14:35:11 +1100

    Fixed issue with Vehicle on ListenServer
    Forget to uncomment a line from when I was debugging.

--- RacingDemo/Source/RacingDemo/PlayerVehicle/PCGVehiclePawn.cpp
+++ RacingDemo/Source/RacingDemo/PlayerVehicle/PCGVehiclePawn.cpp
@@ -42,7 +42,7 @@ void APCGVehiclePawn::BeginPlay()
 	if (GetNetMode() == NM_ListenServer)
 	{
 		UE_LOG(LogTemp, Warning, TEXT("NetMode is ListenServer"))
-		//ProceduralComponent->GenerateRandomVehicle(VehicleRarity, VehicleStats);
+		ProceduralComponent->GenerateRandomVehicle(VehicleRarity, VehicleStats);
 		ApplyWeightDistribution();
 		FuelComponent->SetCurrentFuel(VehicleStats.MaxFuelCapacity);
 
